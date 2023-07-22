6b0dca8d61587cd07f1d4092b6959bb88f2586c0
cefimmac154 <cefim@Armand-Mac.local>
2023-07-22 08:48:06 +0200

    add log for profduction log

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Pagination = ({pagination, setPagination}) => {
-
+    console.log(pagination);
     return (
         <div className='pagination-container'>
             <button
